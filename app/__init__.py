95c2ede52fe7388474ae05e4c1e8cfe0d37b6fb3
Konstantin <astraker98@gmail.com>
2021-04-24 19:36:44 +0300

    main

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
-import app.config as conf
+#import app.config as conf
 app = Flask(__name__)
-app.config.from_object(conf.AppConfig)
+#app.config.from_object(conf.AppConfig)
 from app import routes
 from app import user_routes
